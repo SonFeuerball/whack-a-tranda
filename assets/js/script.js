d52e830c7c96758df4d19edf41b1fa092464d627
SonFeuerball <bobbaumann.home@gmail.com>
2021-04-13 15:10:04 +0200

    the whack actually does something

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,14 @@ let result = 0
 let currentTime = timeLeft.textContent
 
 function randomSquare() {
+
     square.forEach(className => {
         className.classList.remove('tranda')
+        className.classList.remove('squarehittered')
+        className.classList.remove('hitanimation')
     })
     let randomPosition = square[Math.floor(Math.random() * 9)]
     randomPosition.classList.add('tranda')
-
     //randomposition has to be the hit position
     hitPosition = randomPosition.id
 }
@@ -21,15 +23,33 @@ function randomSquare() {
 square.forEach(element => {
     element.addEventListener('mouseup', () => {
         if (element.id === hitPosition) {
+            // playAnimation()
+            element.classList.add('hitanimation')
+            // element.classList.add("squarehittered")
+            element.classList.add('squarehittered')
+            element.classList.remove('tranda')
+            // document.getElementById(element.id).style.background = URL('assets/img/trash_panda_profile_200_dead.png')
+
+            // square.className = "hitanimation"
+            // element.className = "hitanimation"
+
             result = result + 1
             score.textContent = result
         }
     })
 })
 
+// function playAnimation() {
+//     window.requestAnimationFrame(function (time) {
+//         window.requestAnimationFrame(function (time) {
+//             square.classList.add("hitanimation")
+//         })
+//     })
+// }
+
 function moveTranda() {
     let squareTimerId = null
-    squareTimerId = setInterval(randomSquare, 400)
+    squareTimerId = setInterval(randomSquare, 700)
 }
 
 moveTranda()
